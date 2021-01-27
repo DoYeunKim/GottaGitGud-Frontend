9bebc09d54393ec8f75adce97d6f73e8484cd7ad
Do Yeun Kim <doyeunkim.vm@gmail.com>
2021-01-27 10:36:29 -0500

    initialized Angular project with docker

--- gotta-git-gud-frontend/e2e/src/app.e2e-spec.ts
+++ gotta-git-gud-frontend/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('GottaGitGud app is running!');
+    expect(await page.getTitleText()).toEqual('gotta-git-gud-frontend app is running!');
   });
 
   afterEach(async () => {
--- gotta-git-gud-frontend/src/app/app.component.spec.ts
+++ gotta-git-gud-frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'GottaGitGud'`, () => {
+  it(`should have as title 'gotta-git-gud-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('GottaGitGud');
+    expect(app.title).toEqual('gotta-git-gud-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('GottaGitGud app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('gotta-git-gud-frontend app is running!');
   });
 });
